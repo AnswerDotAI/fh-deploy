0e59bacdc7704afc4d1338fc48c67fb31a9da07d
Charles Frye <charles@modal.com>
2024-09-11 13:32:55 -0700

    single file, longer instructions, more comments

--- modal/app.py
+++ modal/app.py
@@ -1,9 +1,31 @@
-from fasthtml.common import *
+from pathlib import Path
 
-fasthtml_app, rt = fast_app()
+import fasthtml.common as fh
+import modal
 
+# create the fasthtml_app object and its rt decorator
+fasthtml_app, rt = fh.fast_app()
+
+
+# define the app first, e.g. adding routes
+# FastHTML uses decorators to add routes:
 @rt("/")
 def get():
-    return Titled("FastHTML", P("FastHTML on Modal!"))
+    return fh.Titled("FastHTML", fh.P("FastHTML on️ Modal!"))
 
-serve(app='fasthtml_app')
+
+if __name__ == "__main__":  # if invoked with `python`, run locally
+    fh.serve(app="fasthtml-app")
+else:  # create a modal app, which can be imported in another file or used with modal commands as in README
+    app = modal.App(name="fasthtml-app")
+
+    # modal uses decorators to define infrastructure and deployments
+    @app.function(  # here's where you can attach GPUs, define concurrency limits, etc.
+        image=modal.Image.debian_slim().pip_install_from_requirements(  # see https://modal.com/docs/guide/custom-container for more ways to install dependencies
+            Path(__file__).parent / "requirements.txt"
+        ),
+        allow_concurrent_inputs=1000,  # async functions can handle multiple inputs
+    )
+    @modal.asgi_app()  # add this decorator to a function that returns your fastHTML app to make it deployable on Modal
+    def serve():
+        return fasthtml_app
